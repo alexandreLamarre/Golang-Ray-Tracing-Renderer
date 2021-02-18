96c36cd95f9da33a92363e147264d53a110db85d
alexandreLamarre <alex7285@gmail.com>
2021-02-18 17:04:11 -0500

    augmented minheap datatype

--- pkg/datatypes/containers.go
+++ pkg/datatypes/containers.go
@@ -1,27 +1,38 @@
 package datatypes
 
-type MinHeap struct{
+type MinHeap struct {
 	container []float64
 }
 
-func NewMinHeap()*MinHeap{
-	res := make([]float64, 0,0)
+func NewMinHeap() *MinHeap {
+	res := make([]float64, 0, 0)
 	return &MinHeap{res}
 }
 
-func (m *MinHeap) Push(el float64){
+func (m *MinHeap) Push(el float64) {
 	m.container = append(m.container, el)
-	heapify(m, len(m.container) -1)
+	heapify(m, len(m.container)-1)
 }
 
-func (m *MinHeap) GetMin()float64{
+func (m *MinHeap) PushAll(el ...float64) {
+	for i := 0; i < len(el); i++ {
+		m.container = append(m.container, el[i])
+		heapify(m, len(m.container)-1)
+	}
+}
+
+func (m *MinHeap) Get() []float64 {
+	return m.container
+}
+
+func (m *MinHeap) GetMin() float64 {
 	return m.container[0]
 }
 
-func heapify(m *MinHeap, i int){
-	parent := (i -2)/2
-	if parent >= 0{
-		if m.container[i]< m.container[parent] {
+func heapify(m *MinHeap, i int) {
+	parent := (i - 2) / 2
+	if parent >= 0 {
+		if m.container[i] < m.container[parent] {
 			temp := m.container[parent]
 			m.container[parent] = m.container[i]
 			m.container[i] = temp
--- pkg/datatypes/containers_test.go
+++ pkg/datatypes/containers_test.go
@@ -1,24 +1,23 @@
 package datatypes
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestMinHeap(t *testing.T) {
 
 	h := NewMinHeap()
-	if len(h.container) != 0{
+	if len(h.container) != 0 {
 		t.Errorf("invalid starting size of heap")
 	}
 
 	h.Push(5)
 
-	if len(h.container) != 1{
+	if len(h.container) != 1 {
 		t.Errorf("invalid size of heap %d, expected: %d", len(h.container), 1)
 	}
 
-	if h.GetMin() != 5.0{
+	if h.GetMin() != 5.0 {
 		t.Errorf("invalid minimum of heap %f, expected %f", h.GetMin(), 5.0)
 	}
 
@@ -27,12 +26,39 @@ func TestMinHeap(t *testing.T) {
 	h.Push(8)
 	h.Push(1)
 
-	if len(h.container) != 5{
+	if len(h.container) != 5 {
 		t.Errorf("invalid size of heap %d, expected: %d", len(h.container), 5)
 	}
-	fmt.Println(h.container)
-	if h.GetMin() != 1.0{
+	if h.GetMin() != 1.0 {
 		t.Errorf("invalid minimum of heap %f, expected %f", h.GetMin(), 1.0)
 	}
+
+	v := h.Get()
+	res := []float64{1, 3, 7, 8, 5}
+	testSliceEquals(t, v, res)
+
+	h.PushAll(-1, 1, 3, 4)
+
+	if len(h.container) != 9 {
+		t.Errorf("invalid size of heap %d, expected: %d", len(h.container), 9)
+	}
+
+	if h.GetMin() != -1.0 {
+		t.Errorf("invalid minimum of heap %f, expected %f", h.GetMin(), -1.0)
+	}
+
+	res = []float64{-1, 1, 1, 4, 5, 3, 7, 3, 8}
+
+	testSliceEquals(t, h.Get(), res)
 }
 
+func testSliceEquals(t *testing.T, values []float64, expected []float64) {
+	if len(values) != len(expected) {
+		t.Errorf("Mismatches lengths: Expected: %d, Got: %d", len(expected), len(values))
+	}
+	for i := 0; i < len(values); i++ {
+		if values[i] != expected[i] {
+			t.Errorf("Expected value at %d : %f, Got: %f", i, expected[i], values[i])
+		}
+	}
+}
