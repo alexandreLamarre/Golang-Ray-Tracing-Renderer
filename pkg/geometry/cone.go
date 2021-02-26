cc4daa860a7b884206e7b4c65dbf8cdc9c9d95be
alexandreLamarre <alex7285@gmail.com>
2021-02-26 17:27:02 -0500

    Added cone shape with minor bug

---
+++ pkg/geometry/cone.go
@@ -0,0 +1,174 @@
+package geometry
+
+import (
+	"github.com/alexandreLamarre/Golang-Ray-Tracing-Renderer/pkg/algebra"
+	"github.com/alexandreLamarre/Golang-Ray-Tracing-Renderer/pkg/canvas"
+	"math"
+)
+
+//Cone defines a default Cone Shape
+type Cone struct{
+	closed bool //determines if the cone is hollow or has caps on the ends
+	transform *algebra.Matrix
+	material *canvas.Material
+	maximum float64 //maximum y-value by default without transformations
+	minimum float64 //minimum y-value by default without transformations
+}
+
+//NewCone returns a new Cone Shape
+func NewCone(m *algebra.Matrix) *Cone{
+	mat := m
+	if m == nil || len(m.Get()) != 4 || len(m.Get()[0]) != 4 {
+		mat = algebra.IdentityMatrix(4)
+	}
+	return &Cone{transform: mat, material: canvas.NewDefaultMaterial(),
+		maximum: math.Inf(1), minimum: math.Inf(-1), closed: false}
+}
+
+//SetMinimum Setter for cylinder minimum y-truncation
+func (cone *Cone) SetMinimum(min float64){
+	cone.minimum = min
+}
+
+//SetMaximum Setter for cylinder maximum y-truncation
+func (cone *Cone) SetMaximum(max float64){
+	cone.maximum = max
+}
+
+//SetClosed Setter for cylinder closededness: whether or not it has caps on the cylinder
+func (cone *Cone) SetClosed(closed bool){
+	cone.closed = closed
+}
+
+//Shape interface methods
+
+//GetTransform Getter for Cylinder Shape transform
+func (cone *Cone) GetTransform() *algebra.Matrix{
+	return cone.transform
+}
+
+//GetMaterial Getter for Cylinder Shape material
+func (cone *Cone) GetMaterial() *canvas.Material{
+	return cone.material
+}
+
+//SetTransform Setter for Cylinder Shape transform
+func (cone *Cone) SetTransform(m *algebra.Matrix){
+	if len(m.Get()) != 4 || len(m.Get()[0]) != 4{
+		panic(algebra.ExpectedDimension(4))
+	}
+	cone.transform = m
+}
+
+//SetMaterial Setter for Cylinder Shape material
+func (cone *Cone) SetMaterial(m *canvas.Material){
+	cone.material = m
+}
+
+//LocalIntersect returns the itersection values for a Ray with a Cylinder
+func (cone *Cone) LocalIntersect(ray *algebra.Ray) ([]float64, bool){
+	direction := ray.Get()["direction"]
+	origin := ray.Get()["origin"]
+	dx := direction.Get()[0]; dy := direction.Get()[1]; dz:= direction.Get()[2]
+	ox := origin.Get()[0]; oy := origin.Get()[1]; oz := origin.Get()[2]
+
+
+	a := (dx * dx) -(dy * dy) + (dz * dz)
+	b := (2 * ox * dx) - (2 * oy * dy) + (2 * oz * dz)
+	c := (ox*ox) - (oy * oy) + (oz * oz)
+
+
+
+	xs := make([]float64, 0, 0)
+	xs = cone.intersectCaps(ray, xs)
+
+	EPSILON := 0.00001
+	if a <= EPSILON && a >= -EPSILON{
+		if !(b <= EPSILON && b >= -EPSILON){
+			xs = append(xs, -c/(2*b))
+			return xs, true
+		}
+
+		var hit bool
+		if len(xs) == 0{
+			hit = false
+		} else{
+			hit = true
+		}
+		return xs, hit
+	}
+
+	disc := b*b - 4 * a *c
+	if disc < 0 {
+		var hit bool
+		if len(xs) == 0{
+			hit = false
+		} else{
+			hit = true
+		}
+		return xs, hit
+	}
+
+	t0 := (-b- math.Sqrt(disc))/(2*a)
+	t1 := (-b + math.Sqrt(disc))/(2*a)
+
+
+	y0 := oy + t0 * dy
+	if cone.minimum < y0 && cone.maximum > y0{
+		xs = append(xs, t0)
+	}
+
+	y1 := oy + t1* dy
+	if cone.minimum < y1 && cone.maximum > y1{
+		xs = append(xs, t1)
+	}
+
+	var hit bool
+	if len(xs) == 0{
+		hit = false
+	} else{
+		hit = true
+	}
+
+	return xs, hit
+}
+
+//LocalNormalAt returns the normal at an intersection point of a Cylinder Shape, shape interface method
+func (cone *Cone) LocalNormalAt(p *algebra.Vector) (*algebra.Vector, error){
+	x := p.Get()[0]; y := p.Get()[1]; z := p.Get()[2]
+	dist := x*x + z * z
+	EPSILON:= 0.001
+	if dist < 1 && y >= cone.maximum - EPSILON{
+		return algebra.NewVector(0, 1, 0), nil
+	} else if dist < 1 && y <= cone.minimum + EPSILON{
+		return algebra.NewVector(0, -1, 0), nil
+	}
+	y = math.Sqrt(dist)
+	if p.Get()[1] > 0 {
+		y = -y
+	}
+	return algebra.NewVector(x, y, z), nil
+}
+
+//cone helpers
+
+func (cone *Cone) intersectCaps(ray *algebra.Ray, xs []float64) []float64{
+	origin := ray.Get()["origin"]
+	direction := ray.Get()["direction"]
+	oy := origin.Get()[1]; dy := direction.Get()[1]
+	EPSILON := 0.0001
+	if !cone.closed || (dy < EPSILON && dy > -EPSILON){
+		return xs
+	}
+
+	t := (cone.minimum -oy)/dy
+	if checkCap(ray , t){
+		xs = append(xs , t)
+	}
+
+	t = (cone.maximum - oy)/dy
+	if checkCap(ray, t){
+		xs = append(xs, t)
+	}
+	return xs
+}
---
+++ pkg/geometry/cone_test.go
@@ -0,0 +1,204 @@
+package geometry
+
+
+import (
+	"github.com/alexandreLamarre/Golang-Ray-Tracing-Renderer/pkg/algebra"
+	"github.com/alexandreLamarre/Golang-Ray-Tracing-Renderer/pkg/canvas"
+	"math"
+	"testing"
+)
+
+func TestNewCone(t *testing.T) {
+	c := NewCone(nil)
+	testMatrixEquals(t, c.transform.Get(), algebra.IdentityMatrix(4).Get())
+	testMaterialEquals(t, c.material, canvas.NewDefaultMaterial())
+	assertEquals(t, c.minimum, math.Inf(-1))
+	assertEquals(t, c.maximum, math.Inf(1))
+	if c.closed {
+		t.Errorf("New cylinders are expected to not be closed by default")
+	}
+}
+
+func TestCone_SetMinimum(t *testing.T) {
+	c := NewCone(nil)
+	c.SetMinimum(2)
+	assertEquals(t, c.minimum, 2)
+}
+
+func TestCone_SetMaximum(t *testing.T) {
+	c := NewCone(nil)
+	c.SetMaximum(1)
+	assertEquals(t, c.maximum, 1)
+}
+
+func TestCone_SetClosed(t *testing.T) {
+	c := NewCone(nil)
+	c.SetClosed(true)
+	if !c.closed{
+		t.Errorf("Expected cylinder to be closed")
+	}
+	c.SetClosed(false)
+	if c.closed{
+		t.Errorf("Expected cylinder to not be closed")
+	}
+}
+
+func TestCone_GetMaterial(t *testing.T) {
+	c := NewCone(nil)
+	m := c.GetMaterial()
+	testMaterialEquals(t, m, canvas.NewDefaultMaterial())
+}
+
+func TestCone_GetTransform(t *testing.T) {
+	c := NewCone(nil)
+	m := c.GetTransform()
+	testMatrixEquals(t, m.Get(), algebra.IdentityMatrix(4).Get())
+}
+
+func TestCone_SetMaterial(t *testing.T) {
+	c := NewCone(nil)
+	m := canvas.NewDefaultMaterial()
+	m.Specular = 10
+	c.SetMaterial(m)
+	testMaterialEquals(t, c.material, m)
+}
+
+func TestCone_SetTransform(t *testing.T) {
+	c := NewCone(nil)
+	m := algebra.ScalingMatrix(2,2,2)
+	c.SetTransform(m)
+	testMatrixEquals(t, m.Get(), c.transform.Get())
+}
+
+func TestCone_LocalIntersect(t *testing.T) {
+	c := NewCone(nil)
+	norm, err := algebra.NewVector(0, 0, 1).Normalize()
+	if err != nil{
+		t.Errorf("%s", err)
+	}
+	norm2, err := algebra.NewVector(1, 1, 1).Normalize()
+	if err != nil{
+		t.Errorf("%s", err)
+	}
+	norm3 , err:= algebra.NewVector(-0.5, -1, 1).Normalize()
+	if err != nil{
+		t.Errorf("%s", err)
+	}
+
+	rays := []*algebra.Ray{
+		algebra.NewRay(0, 0, -5, norm.Get()[0], norm.Get()[1], norm.Get()[2]),
+		algebra.NewRay(0, 0, -5, norm2.Get()[0], norm2.Get()[1], norm2.Get()[2]),
+		algebra.NewRay(1, 1, -5, norm3.Get()[0], norm3.Get()[1], norm3.Get()[2]),
+	}
+
+	positions := [][2]float64{
+		{5, 5},
+		{8.66025, 8.66025},
+		{4.55006, 49.44994},
+	}
+
+	for i := 0; i < len(rays); i++{
+		xs, hit := c.LocalIntersect(rays[i])
+		if !hit{
+			t.Errorf("Expected ray %v, %v to not hit the cone",
+				rays[i].Get()["origin"], rays[i].Get()["direction"])
+		}
+		if len(xs) != 2{
+			t.Errorf("Expected %d hits, got: %d", 2, len(xs))
+		}
+		if !equals(xs[0],positions[i][0]){
+			t.Errorf("Expected intersection position: %f, Got: %f", positions[i][0], xs[0])
+		}
+
+		if !equals(xs[1], positions[i][1]){
+			t.Errorf("Expected intersection position: %f, Got: %f", positions[i][1], xs[1])
+		}
+	}
+
+	//ray intersect parallel to one of the halves
+	norm,err = algebra.NewVector(0, 1, 1).Normalize()
+	if err != nil{
+		t.Errorf("%s", err)
+	}
+	r := algebra.NewRay(0, 0, -1, norm.Get()[0], norm.Get()[1], norm.Get()[2])
+	xs, hit := c.LocalIntersect(r)
+
+	if !hit{
+		t.Errorf("Expected ray %v, %v to hit the cylinder",
+			r.Get()["origin"], r.Get()["direction"])
+	}
+
+	if len(xs) != 1{
+		t.Errorf("Expected %d hits, got: %d", 1, len(xs))
+	}
+
+	if !equals(xs[0], 0.35355){
+		t.Errorf("Expected intersection at %f, Got: %f", 0.35355, xs[0])
+	}
+
+	//intersect the end caps
+
+	c = NewCone(nil)
+	c.minimum = -0.5
+	c.maximum = 0.5
+	c.closed = true
+
+	norm, err = algebra.NewVector(0, 1, 0).Normalize()
+	norm2, err = algebra.NewVector(0, 1, 1).Normalize()
+	norm3, err = algebra.NewVector(0 , 1, 0).Normalize()
+
+	rays = []*algebra.Ray{
+		algebra.NewRay(0, 0, -5, norm.Get()[0], norm.Get()[1], norm.Get()[2]),
+		algebra.NewRay(0, 0, -0.25, norm.Get()[0], norm.Get()[1], norm.Get()[2]),
+		algebra.NewRay(0, 0, -0.25, norm.Get()[0], norm.Get()[1], norm.Get()[2]),
+	}
+
+	for i := 0; i < len(rays); i++{
+		xs, hit := c.LocalIntersect(rays[i])
+		if i == 0{
+			if hit{
+				t.Errorf("Expected ray %v, %v to not hit the cylinder",
+					rays[i].Get()["origin"], rays[i].Get()["direction"])
+			}
+			if len(xs) != 0{
+				t.Errorf("Expected %d hits, got: %d", 0, len(xs))
+			}
+
+		} else{
+			if !hit{
+				t.Errorf("Expected ray %v, %v to hit the cylinder",
+					rays[i].Get()["origin"], rays[i].Get()["direction"])
+			}
+			if i == 1 && len(xs) != 2{
+				t.Errorf("Expected %d hits, got: %d", 2, len(xs))
+			}
+			if i ==2 && len(xs) != 4{
+				t.Errorf("Expected %d hits, got: %d", 4, len(xs))
+			}
+		}
+	}
+}
+
+func TestCone_LocalNormalAt(t *testing.T) {
+	c := NewCone(nil)
+
+	points := []*algebra.Vector{
+		algebra.NewPoint(0, 0, 0),
+		algebra.NewPoint(1, 1, 1),
+		algebra.NewPoint(-1, -1, 0),
+	}
+
+	normals := []*algebra.Vector{
+		algebra.NewVector(0 , 0, 0),
+		algebra.NewVector(1, -math.Sqrt(2), 1),
+		algebra.NewVector(-1, 1, 0),
+	}
+
+	for i := 0; i < len(points); i++{
+		n,err := c.LocalNormalAt(points[i])
+		if err != nil{
+			t.Errorf("%s", err)
+		}
+		testVectorEquals(t, n.Get(), normals[i].Get())
+	}
+}
