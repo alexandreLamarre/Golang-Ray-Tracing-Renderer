e7f194eda0063bd95f4f7e478e5f1701fa483cb7
alexandreLamarre <alex7285@gmail.com>
2021-03-03 19:47:09 -0500

    reformat with go fmt

--- pkg/geometry/objects/CSG.go
+++ pkg/geometry/objects/CSG.go
@@ -13,47 +13,48 @@ import (
 //called
 type ErrorCSGNormal int
 
-func (e ErrorCSGNormal) Error() string{
+func (e ErrorCSGNormal) Error() string {
 	return fmt.Sprintf("CSG Local normal called : Invalid use of LocalNormal method")
 }
 
 //Contains Check if the Shape is the provided Shape/contains the provided Shape
-func Contains(s primitives.Shape, testContained primitives.Shape) bool{
-	if reflect.TypeOf(s) == reflect.TypeOf(&primitives.Group{}){
+func Contains(s primitives.Shape, testContained primitives.Shape) bool {
+	if reflect.TypeOf(s) == reflect.TypeOf(&primitives.Group{}) {
 		g := s.(*primitives.Group)
-		for _, v := range g.GetShapes(){
-			if Contains(v, testContained){return true}
+		for _, v := range g.GetShapes() {
+			if Contains(v, testContained) {
+				return true
+			}
 		}
-	} else if reflect.TypeOf(s) == reflect.TypeOf(&CSGShape{}){
+	} else if reflect.TypeOf(s) == reflect.TypeOf(&CSGShape{}) {
 		csg := s.(*CSGShape)
-		if Contains(csg.Left(), testContained){
+		if Contains(csg.Left(), testContained) {
 			return true
 		}
-		if Contains(csg.Right(), testContained){
+		if Contains(csg.Right(), testContained) {
 			return true
 		}
 	}
 	return s == testContained
 }
 
-
 //CSGShape is the datatype that uses the Constructive Geometry
 // approach to handling groups of objects
-type CSGShape struct{
-	Parent primitives.Shape
-	material *canvas.Material
+type CSGShape struct {
+	Parent    primitives.Shape
+	material  *canvas.Material
 	transform *algebra.Matrix
-	left primitives.Shape
-	right primitives.Shape
-	action string
+	left      primitives.Shape
+	right     primitives.Shape
+	action    string
 }
 
 //UnionCSG initializes a CSG shape based on the union operation of two Shapes: left and right
-func UnionCSG(left, right primitives.Shape) *CSGShape{
-	if left == nil{
+func UnionCSG(left, right primitives.Shape) *CSGShape {
+	if left == nil {
 		panic("Undefined left Shape in CSG operation `union`")
 	}
-	if right == nil{
+	if right == nil {
 		panic("Undefined right Shape in CSG operation `union`")
 	}
 
@@ -65,11 +66,11 @@ func UnionCSG(left, right primitives.Shape) *CSGShape{
 }
 
 //IntersectCSG initializes a CSG shape based on the union operation of two Shapes: left and right
-func IntersectCSG(left, right primitives.Shape) *CSGShape{
-	if left == nil{
+func IntersectCSG(left, right primitives.Shape) *CSGShape {
+	if left == nil {
 		panic("Undefined left Shape in CSG operation `intersect`")
 	}
-	if right == nil{
+	if right == nil {
 		panic("Undefined right Shape in CSG operation `intersect`")
 	}
 	c := &CSGShape{Parent: nil, material: canvas.NewDefaultMaterial(),
@@ -80,11 +81,11 @@ func IntersectCSG(left, right primitives.Shape) *CSGShape{
 }
 
 //DifferenceCSG initializes a CSG shape based on the union operation of two Shapes: left and right
-func DifferenceCSG(left, right primitives.Shape) *CSGShape{
-	if left == nil{
+func DifferenceCSG(left, right primitives.Shape) *CSGShape {
+	if left == nil {
 		panic("Undefined left Shape in CSG operation `difference`")
 	}
-	if right == nil{
+	if right == nil {
 		panic("Undefined right Shape in CSG operation `difference`")
 	}
 	c := &CSGShape{Parent: nil, material: canvas.NewDefaultMaterial(),
@@ -95,44 +96,48 @@ func DifferenceCSG(left, right primitives.Shape) *CSGShape{
 }
 
 //Left returns the left shape of the CSG Shape
-func (s *CSGShape) Left() primitives.Shape{
+func (s *CSGShape) Left() primitives.Shape {
 	return s.left
 }
 
 //Right returns the left shape of the CSG Shape
-func (s *CSGShape) Right() primitives.Shape{
+func (s *CSGShape) Right() primitives.Shape {
 	return s.right
 }
 
 //primitives.Shape interface methods
 
 //GetTransform Getter for CSG Shape transform, primitives.Shape interface method
-func (s *CSGShape) GetTransform() *algebra.Matrix{
+func (s *CSGShape) GetTransform() *algebra.Matrix {
 	return s.transform
 }
 
 //GetMaterial Getter for CSG Shape material, primitives.Shape interface method
-func (s *CSGShape) GetMaterial() *canvas.Material{
+func (s *CSGShape) GetMaterial() *canvas.Material {
 	return s.material
 }
 
 //GetParent Getter for CSG Parent Shape, primitives.Shape interface method
-func (s *CSGShape) GetParent() primitives.Shape{
+func (s *CSGShape) GetParent() primitives.Shape {
 	return s.Parent
 }
 
 //GetBounds Getter for CSG Shape bounding box, primitives.Shape interface method
-func (s *CSGShape) GetBounds() (*algebra.Vector, *algebra.Vector){
+func (s *CSGShape) GetBounds() (*algebra.Vector, *algebra.Vector) {
 
-	minL, maxL :=  s.left.GetBounds()
-	minR, maxR :=  s.right.GetBounds()
+	minL, maxL := s.left.GetBounds()
+	minR, maxR := s.right.GetBounds()
 	bL := primitives.GetBoundsTransform(minL, maxL, s.left.GetTransform())
 	br := primitives.GetBoundsTransform(minL, maxL, s.right.GetTransform())
 	minL, maxL = bL.Get()
 	minR, maxR = br.Get()
 
-	minX := math.Inf(1); minY := math.Inf(1); minZ := math.Inf(1);
-	maxX := math.Inf(-1); maxY := math.Inf(-1); maxZ := math.Inf(-1);
+	minX := math.Inf(1)
+	minY := math.Inf(1)
+	minZ := math.Inf(1)
+	maxX := math.Inf(-1)
+	maxY := math.Inf(-1)
+	maxZ := math.Inf(-1)
 
 	minX = math.Min(minX, minL.Get()[0])
 	minX = math.Min(minX, minR.Get()[0])
@@ -153,53 +158,53 @@ func (s *CSGShape) GetBounds() (*algebra.Vector, *algebra.Vector){
 }
 
 //SetTransform Setter for CSG Shape transform, primitives.Shape interface method
-func (s *CSGShape)SetTransform(m *algebra.Matrix){
-	if len(m.Get()) != 4 || len(m.Get()[0]) != 4{
+func (s *CSGShape) SetTransform(m *algebra.Matrix) {
+	if len(m.Get()) != 4 || len(m.Get()[0]) != 4 {
 		panic(algebra.ExpectedDimension(4))
 	}
 	s.transform = m
 }
 
 //SetMaterial Setter for CSG Shape material, primitives.Shape interface method
-func (s *CSGShape) SetMaterial(m *canvas.Material){
+func (s *CSGShape) SetMaterial(m *canvas.Material) {
 	s.material = m
 }
 
 //SetParent Setter for CSG Shape parent, primitives.Shape interface method
-func (s *CSGShape) SetParent(shape primitives.Shape){
+func (s *CSGShape) SetParent(shape primitives.Shape) {
 	s.Parent = shape
 }
 
 //LocalIntersect Intersect implementation for CSG Shape, Check intersections of left and right
 //and apply the appropriate CSG Shape action. primitives.Shape interface method.
-func (s *CSGShape)LocalIntersect(r *algebra.Ray) ([]*primitives.Intersection, bool){
+func (s *CSGShape) LocalIntersect(r *algebra.Ray) ([]*primitives.Intersection, bool) {
 	xs := make([]*primitives.Intersection, 0, 0)
 	is := primitives.NewIntersections()
 	err := is.Intersect(s.Left(), r)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	err = is.Intersect(s.Right(), r)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 
-	if is.Count() == 0{
+	if is.Count() == 0 {
 		return xs, false
 	}
 	hits := is.GetHits()
 	refs := is.GetRef()
 
 	k := len(refs.Get())
-	for i := 0; i < k; i++{
-		if min:= refs.ExtractMin(); min != nil{
+	for i := 0; i < k; i++ {
+		if min := refs.ExtractMin(); min != nil {
 			xs = append(xs, refs.ExtractMin())
 		}
 	}
 
 	k = len(hits.Get())
-	for i := 0; i < k; i++{
-		if min := hits.ExtractMin(); min != nil{
+	for i := 0; i < k; i++ {
+		if min := hits.ExtractMin(); min != nil {
 			xs = append(xs, min)
 		}
 	}
@@ -210,25 +215,25 @@ func (s *CSGShape)LocalIntersect(r *algebra.Ray) ([]*primitives.Intersection, bo
 
 //LocalNormalAt Local normal implementation for CSG Shape, should never be directly called on. Only
 //the children of CSG shape should be intersected. primitives.Shape interface method.
-func (s *CSGShape) LocalNormalAt(p *algebra.Vector, hit *primitives.Intersection) (*algebra.Vector, error){
+func (s *CSGShape) LocalNormalAt(p *algebra.Vector, hit *primitives.Intersection) (*algebra.Vector, error) {
 	return nil, ErrorCSGNormal(0)
 }
 
 // CSG Shape helpers
 
-func (s *CSGShape) filterIntersections(xs []*primitives.Intersection) []*primitives.Intersection{
+func (s *CSGShape) filterIntersections(xs []*primitives.Intersection) []*primitives.Intersection {
 	inl := false
 	inr := false
 
 	result := make([]*primitives.Intersection, 0, 0)
 
-	for _, i := range xs{
+	for _, i := range xs {
 		lhit := Contains(s.Left(), i.Object)
-		if intersectionAllowed(s.action, lhit, inl, inr){
+		if intersectionAllowed(s.action, lhit, inl, inr) {
 			result = append(result, i)
 		}
 
-		if lhit{
+		if lhit {
 			inl = !(inl)
 		} else {
 			inr = !(inr)
@@ -237,15 +242,14 @@ func (s *CSGShape) filterIntersections(xs []*primitives.Intersection) []*primiti
 	return result
 }
 
-
-func intersectionAllowed(action string, lhit, inl, inr bool) bool{
-	if action == "union"{
+func intersectionAllowed(action string, lhit, inl, inr bool) bool {
+	if action == "union" {
 		return (lhit && (!inr)) || ((!lhit) && (!inl))
 	}
-	if action == "intersect"{
+	if action == "intersect" {
 		return (lhit && inr) || ((!lhit) && inl)
 	}
-	if action == "difference"{
+	if action == "difference" {
 		return (lhit && !inr) || (!lhit && inl)
 	}
 	return false
--- pkg/geometry/objects/CSG_test.go
+++ pkg/geometry/objects/CSG_test.go
@@ -16,38 +16,36 @@ func TestContains(t *testing.T) {
 	g.AddChild(s1)
 	g.AddChild(s2)
 
-	if !Contains(s1, s1){
+	if !Contains(s1, s1) {
 		t.Errorf("Expected s1 to trivially contain s1")
 	}
 
-	if Contains(s2, s1){
+	if Contains(s2, s1) {
 		t.Errorf("Expected s2 to not trivially contain s1")
 	}
 
-	if !Contains(g, s1){
+	if !Contains(g, s1) {
 		t.Errorf("Expected group g to contain s1")
 	}
 
-	if Contains(g, s3){
+	if Contains(g, s3) {
 		t.Errorf("Expected group g to not contain s3")
 	}
-
 
 	g1 := primitives.NewGroup(nil)
 	g1.AddChild(g)
 
-
-	if !Contains(g1, s1){
+	if !Contains(g1, s1) {
 		t.Errorf("Expected group g to contain s1")
 	}
 
-	if Contains(g1, s3){
+	if Contains(g1, s3) {
 		t.Errorf("Expected group g to not contain s3")
 	}
 
 	g1.AddChild(s3)
 
-	if !Contains(g1, s3){
+	if !Contains(g1, s3) {
 		t.Errorf("Expected group g1 to contain s3")
 	}
 
@@ -55,37 +53,37 @@ func TestContains(t *testing.T) {
 	s5 := primitives.NewSphere(nil)
 	csg := UnionCSG(s4, s5)
 
-	if Contains(csg, s1){
+	if Contains(csg, s1) {
 		t.Errorf("Expected csg")
 	}
 
-	if !Contains(csg, s4){
+	if !Contains(csg, s4) {
 		t.Errorf("Expected csg to contain s4")
 	}
 
-	if !Contains(csg, s5){
+	if !Contains(csg, s5) {
 		t.Errorf("Expected csg to contain s5")
 	}
 
 	g.AddChild(csg)
 
-	if !Contains(g1, s4){
+	if !Contains(g1, s4) {
 		t.Errorf("Expected g1 to contain s5")
 	}
 
-	if !Contains(g1, s5){
+	if !Contains(g1, s5) {
 		t.Errorf("Expected g1 to contain s5")
 	}
 	s6 := primitives.NewSphere(nil)
 	csg2 := IntersectCSG(csg, s6)
-	if !Contains(csg2, s6){
+	if !Contains(csg2, s6) {
 		t.Errorf("Expected csg2 to contain s6")
 	}
 
 	s1 = primitives.NewSphere(nil)
 	s2 = primitives.NewSphere(algebra.TranslationMatrix(0, 0, 0.5))
 	csg = UnionCSG(s1, s2)
-	if Contains(csg.Left(), s2){
+	if Contains(csg.Left(), s2) {
 		t.Errorf("Left side of union should not contain right side")
 	}
 }
@@ -95,13 +93,13 @@ func TestUnionCSG(t *testing.T) {
 	s2 := primitives.NewSphere(nil)
 	csg := UnionCSG(s1, s2)
 
-	if csg.left != s1{
+	if csg.left != s1 {
 		t.Errorf("Expected csg.left to be s1")
 	}
-	if csg.right != s2{
+	if csg.right != s2 {
 		t.Errorf("Expected csg.right to be s2")
 	}
-	if csg.action != "union"{
+	if csg.action != "union" {
 		t.Errorf("Expected union action")
 	}
 }
@@ -111,13 +109,13 @@ func TestDifferenceCSG(t *testing.T) {
 	s2 := primitives.NewSphere(nil)
 	csg := DifferenceCSG(s1, s2)
 
-	if csg.left != s1{
+	if csg.left != s1 {
 		t.Errorf("Expected csg.left to be s1")
 	}
-	if csg.right != s2{
+	if csg.right != s2 {
 		t.Errorf("Expected csg.right to be s2")
 	}
-	if csg.action != "difference"{
+	if csg.action != "difference" {
 		t.Errorf("Expected union action")
 	}
 }
@@ -127,13 +125,13 @@ func TestIntersectCSG(t *testing.T) {
 	s2 := primitives.NewSphere(nil)
 	csg := IntersectCSG(s1, s2)
 
-	if csg.left != s1{
+	if csg.left != s1 {
 		t.Errorf("Expected csg.left to be s1")
 	}
-	if csg.right != s2{
+	if csg.right != s2 {
 		t.Errorf("Expected csg.right to be s2")
 	}
-	if csg.action != "intersect"{
+	if csg.action != "intersect" {
 		t.Errorf("Expected union action")
 	}
 }
@@ -141,9 +139,9 @@ func TestIntersectCSG(t *testing.T) {
 func TestCSGShape_Left(t *testing.T) {
 	s1 := primitives.NewSphere(nil)
 	s2 := primitives.NewSphere(nil)
-	csg := UnionCSG(s1 , s2)
+	csg := UnionCSG(s1, s2)
 
-	if csg.Left() != csg.left{
+	if csg.Left() != csg.left {
 		t.Errorf("Left doesnt behave appropriately")
 	}
 }
@@ -151,9 +149,9 @@ func TestCSGShape_Left(t *testing.T) {
 func TestCSGShape_Right(t *testing.T) {
 	s1 := primitives.NewSphere(nil)
 	s2 := primitives.NewSphere(nil)
-	csg := UnionCSG(s1 , s2)
+	csg := UnionCSG(s1, s2)
 
-	if csg.Right() != csg.right{
+	if csg.Right() != csg.right {
 		t.Errorf("Left doesnt behave appropriately")
 	}
 }
@@ -186,7 +184,7 @@ func TestCSGShape_GetParent(t *testing.T) {
 	s1 := primitives.NewSphere(nil)
 	s2 := primitives.NewSphere(nil)
 	csg := UnionCSG(s1, s2)
-	if csg.Parent != nil{
+	if csg.Parent != nil {
 		t.Errorf("Expected default parent to be nil")
 	}
 }
@@ -206,7 +204,7 @@ func TestCSGShape_SetParent(t *testing.T) {
 	s2 := primitives.NewSphere(nil)
 	csg := UnionCSG(s1, s2)
 	csg.SetParent(primitives.NewGroup(nil))
-	if csg.GetParent() == nil{
+	if csg.GetParent() == nil {
 		t.Errorf("Expected parent to not be nil")
 	}
 }
@@ -215,11 +213,11 @@ func TestCSGShape_SetTransform(t *testing.T) {
 	s1 := primitives.NewSphere(nil)
 	s2 := primitives.NewSphere(nil)
 	csg := UnionCSG(s1, s2)
-	csg.SetTransform(algebra.ScalingMatrix(2,2,2))
+	csg.SetTransform(algebra.ScalingMatrix(2, 2, 2))
 	testMatrixEquals(t, csg.GetTransform().Get(), algebra.ScalingMatrix(2, 2, 2).Get())
 }
 
-func Test_intersectionAllowed(t *testing.T){
+func Test_intersectionAllowed(t *testing.T) {
 	truthTable := [][]bool{
 		{true, true, true},
 		{true, true, false},
@@ -236,18 +234,18 @@ func Test_intersectionAllowed(t *testing.T){
 
 	differenceResult := []bool{false, true, false, true, true, true, false, false}
 
-	for i, truthVals := range truthTable{
+	for i, truthVals := range truthTable {
 
-		if intersectionAllowed("union", truthVals[0], truthVals[1], truthVals[2]) != unionResult[i]{
+		if intersectionAllowed("union", truthVals[0], truthVals[1], truthVals[2]) != unionResult[i] {
 
 			t.Errorf("Expected union intersection to be %v, for lhit:%v, inl :%v, inr : %v", unionResult[i],
 				truthVals[0], truthVals[1], truthVals[2])
 		}
-		if intersectionAllowed("intersect", truthVals[0], truthVals[1], truthVals[2]) != intersectionResult[i]{
+		if intersectionAllowed("intersect", truthVals[0], truthVals[1], truthVals[2]) != intersectionResult[i] {
 			t.Errorf("Expected intersect intersection to be %v, for lhit:%v, inl :%v, inr : %v", intersectionResult[i],
 				truthVals[0], truthVals[1], truthVals[2])
 		}
-		if intersectionAllowed("difference", truthVals[0], truthVals[1], truthVals[2]) != differenceResult[i]{
+		if intersectionAllowed("difference", truthVals[0], truthVals[1], truthVals[2]) != differenceResult[i] {
 			t.Errorf("Expected difference intersection to be %v, for lhit:%v, inl :%v, inr : %v", differenceResult[i],
 				truthVals[0], truthVals[1], truthVals[2])
 		}
@@ -255,11 +253,11 @@ func Test_intersectionAllowed(t *testing.T){
 
 }
 
-func Test_filterIntersections(t *testing.T){
+func Test_filterIntersections(t *testing.T) {
 	s1 := primitives.NewSphere(nil)
 	s2 := primitives.NewCube(nil)
 	xs := []*primitives.Intersection{
-		primitives.NewIntersection(s1,1),
+		primitives.NewIntersection(s1, 1),
 		primitives.NewIntersection(s2, 2),
 		primitives.NewIntersection(s1, 3),
 		primitives.NewIntersection(s2, 4),
@@ -268,14 +266,14 @@ func Test_filterIntersections(t *testing.T){
 	action := "union"
 	csg := UnionCSG(s1, s2)
 	res := csg.filterIntersections(xs)
-	if len(res) != 2{
+	if len(res) != 2 {
 		t.Errorf("Expected only 2 intersections for %s shape", action)
 		return
 	}
-	if res[0] != xs[0]{
+	if res[0] != xs[0] {
 		t.Errorf("Expected intersection at %v for %s shape, got: %v", xs[0], action, res[0])
 	}
-	if res[1] != xs[3]{
+	if res[1] != xs[3] {
 		t.Errorf("Expected intersection at %v for %s shape, got: %v", xs[3], action, res[1])
 	}
 
@@ -283,14 +281,14 @@ func Test_filterIntersections(t *testing.T){
 	csg = IntersectCSG(s1, s2)
 
 	res = csg.filterIntersections(xs)
-	if len(res) != 2{
+	if len(res) != 2 {
 		t.Errorf("Expected only 2 intersections for %s shape", action)
 		return
 	}
-	if res[0] != xs[1]{
+	if res[0] != xs[1] {
 		t.Errorf("Expected intersection at %v for %s shape, got: %v", xs[1], action, res[0])
 	}
-	if res[1] != xs[2]{
+	if res[1] != xs[2] {
 		t.Errorf("Expected intersection at %v for %s shape, got: %v", xs[2], action, res[1])
 	}
 
@@ -298,14 +296,14 @@ func Test_filterIntersections(t *testing.T){
 	csg = DifferenceCSG(s1, s2)
 
 	res = csg.filterIntersections(xs)
-	if len(res) != 2{
+	if len(res) != 2 {
 		t.Errorf("Expected only 2 intersections for %s shape", action)
 		return
 	}
-	if res[0] != xs[0]{
+	if res[0] != xs[0] {
 		t.Errorf("Expected intersection at %v for %s shape, got: %v", xs[0], action, res[0])
 	}
-	if res[1] != xs[1]{
+	if res[1] != xs[1] {
 		t.Errorf("Expected intersection at %v for %s shape, got: %v", xs[1], action, res[1])
 	}
 }
@@ -314,10 +312,10 @@ func TestCSGShape_LocalIntersect(t *testing.T) {
 	c := UnionCSG(primitives.NewSphere(nil), primitives.NewCube(nil))
 	r := algebra.NewRay(0, 2, -5, 0, 0, 1)
 	xs, hit := c.LocalIntersect(r)
-	if hit{
+	if hit {
 		t.Errorf("Expected ray %v %v to not hit", r.Get()["origin"], r.Get()["direction"])
 	}
-	if len(xs) != 0{
+	if len(xs) != 0 {
 		t.Errorf("Expected ray %v %v to have %d intersections, got : %d",
 			r.Get()["origin"], r.Get()["direction"], 0, len(xs))
 	}
@@ -327,11 +325,11 @@ func TestCSGShape_LocalIntersect(t *testing.T) {
 	csg := UnionCSG(s1, s2)
 	r = algebra.NewRay(0, 0, -5, 0, 0, 1)
 	xs, hit = csg.LocalIntersect(r)
-	if !hit{
+	if !hit {
 		t.Errorf("Expected ray %v %v to hit", r.Get()["origin"], r.Get()["direction"])
 		return
 	}
-	if len(xs) != 2{
+	if len(xs) != 2 {
 		t.Errorf("Expected ray %v %v to have %d intersections, got : %d",
 			r.Get()["origin"], r.Get()["direction"], 2, len(xs))
 	}
@@ -341,14 +339,14 @@ func TestCSGShape_LocalNormalAt(t *testing.T) {
 	s1 := primitives.NewSphere(nil)
 	s2 := primitives.NewSphere(nil)
 	csg := IntersectCSG(s1, s2)
-	n, err := csg.LocalNormalAt(algebra.NewPoint(-1,0,1), primitives.NewIntersection(s1, 1))
-	if n != nil{
+	n, err := csg.LocalNormalAt(algebra.NewPoint(-1, 0, 1), primitives.NewIntersection(s1, 1))
+	if n != nil {
 		t.Errorf("Expected local normal at csg to always return nil")
 	}
-	if err == nil{
+	if err == nil {
 		t.Errorf("Expected error to be no nil")
 	}
-	if serr, _ := err.(*ErrorCSGNormal); serr != nil{
+	if serr, _ := err.(*ErrorCSGNormal); serr != nil {
 		t.Errorf("Expected custom Error 'ErrorCSGNormal', instead got : %s", serr)
 	}
 }
